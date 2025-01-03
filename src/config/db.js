89062b66f153e98342f36a0279665dcea62f09ad
jrvscm <chrisjarvisdev@gmail.com>
2025-01-03 13:02:13 -0700

    update sequelize init

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+    process.env.DATABASE_URL || {
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
-        logging: console.log
+        dialectOptions: process.env.NODE_ENV === 'production' ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        } : {},
     }
 );
 
